eee05984ec49cd2863755748b8d9f3d439f91b35
Parker Ferguson <parkcity.pf@gmail.com>
2021-08-03 15:38:44 -0700

    Added new logo, altered some colors. Location working on submit (console.log). Ready for navigation.

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-//import WelcomeScreen from './Screens/WelcomeScreen';
-//import ViewImageScreen from './Screens/ViewImageScreen';
-//import CardScreen from './Screens/CardScreen';
-//import MessageScreen from './Screens/MessageScreen';
-//import AccountScreen from './Screens/AccountScreen';
-//import ListingsScreen from './Screens/ListingsScreen';
-//import AppTextInput from './components/AppTextInput';
-//import LoginScreen from './Screens/LoginScreen';
-//import RegisterScreen from './Screens/RegisterScreen';
+import WelcomeScreen from './Screens/WelcomeScreen';
+import ViewImageScreen from './Screens/ViewImageScreen';
+import CardScreen from './Screens/CardScreen';
+import MessageScreen from './Screens/MessageScreen';
+import AccountScreen from './Screens/AccountScreen';
+import ListingsScreen from './Screens/ListingsScreen';
+import LoginScreen from './Screens/LoginScreen';
+import RegisterScreen from './Screens/RegisterScreen';
 import ListingEditScreen from './Screens/ListingEditScreen';
 
 export default function App() {
-  //return <WelcomeScreen/>;
+  return <WelcomeScreen/>;
   //return <ViewImageScreen/>;
   //return <CardScreen/>;
   //return <MessageScreen/>;
@@ -20,6 +19,6 @@ export default function App() {
   //return <ListingsScreen/>;
   //return <LoginScreen/>
   //return <RegisterScreen/>
-  return <ListingEditScreen/>
+  //return <ListingEditScreen/>
 
 }
